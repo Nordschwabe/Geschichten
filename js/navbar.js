185c7451dc8ab509ece75dc27f63d7df662b556c
Nordschwabe <Nordschwabe@protonmail.com>
2020-06-27 19:54:25 +0200

    adding js folder

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+const load = () => {
+    // alert(document.getElementById("insertedNavbar").innerHTML);
+    document.getElementById("insertedNavbar").innerHTML =    `   <nav class="navigation">
+    <div class="vertical-center">
+        <ul class="link-list">
+            <li><a href="index.html">Zum Grundstück</a></li>
+            <li><a href="gartenfreuden.html">Gartenfreuden</a></li>
+            <li><a href="pamphlete.html">Subtile Kritik</a></li>
+        </ul> 
+    </div>
+</nav>`;
+}
+load();
