81420b15d06d4831b3456cc2646b64c783f5e86a
Nordschwabe himself <nordschwabe@pm.me>
2020-09-20 22:51:20 +0200

    erste Kurzgeschichte

---
+++ 0_sections/js/navbar_short.js
@@ -0,0 +1,12 @@
+const loadNavbar = () => {
+    document.getElementById("insertedNavbar").innerHTML =    `   <nav class="navigation">
+    <div class="vertical-center">
+        <ul class="link-list">
+            <li><a href="../../html/index.html">Zum Grundstück</a></li>
+            <li><a href="../../0_sections/html/kurzgeschichten.html">Kurzgeschichten</a></li>
+            <li><a href="../../html/latest.html">Das Neueste</a></li>
+        </ul> 
+    </div>
+</nav>`;
+}
+loadNavbar();
